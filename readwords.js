bc048770ef4588931475f3ccf167c75535e546d2
Abhi5Pande <abhi.pachpande@gmail.com>
2021-04-22 14:39:49 +0530

    new pack

--- readwords.js
+++
@@ -1,6 +0,0 @@
-var censor  = require("censorifyv");
-console.log(censor.getCensoredWord());
-console.log(censor.censor("Some very sad , bad , bad text."));
-censor.addCensoredWord("gloomy");
-console.log(censor.getCensoredWord);
-console.log("today is avery gloomy day");
